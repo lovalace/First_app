f9b8a119171bda9fa9b8a8917e6cd38c2a068460
lovalace <erkankaratepe@msn.com>
2019-12-25 02:06:04 +0300

    _productWidget

--- lib/aboutus.dart
+++ lib/aboutus.dart
@@ -12,15 +12,15 @@ class AboutUs extends StatelessWidget {
           child: Column(
             children: <Widget>[
               _stackAboutUs(),
-              _ctText1(),
-              _ctText2(),
-              _ctText7(),
-              _ctText3(),
+              _container('6 kıtaya ihracat yapan Paksan Türkiye’de ve dünyada yatırımlarına hız kesmeden devam ediyor.', _textStyle32),
+              _container('1970 yılında Özhan Pak ve ortakları tarafından kurulmuş olan Paksan Makina bugün Türkiye’nin ve dünyanın en önemli balya makinası üreticileri arasında yer almaktadır. Paksan toplamda 21.000 m2 kapalı toplam 62.000 m2’yi bulan üretim tesislerinde, robotlar, modern CNC tezgahları, son model lazer kesim makinaları ve en gelişmiş çizim programları gibi son teknoloji imkanlarını kullanarak dünya standartlarında makina üretmektedir.', _textStyle14),
+              _container('Paksan her geçen gün ürün yelpazesini geliştirmeye devam etmektedir. Prizmatik balya makinası üretiminin yanı sıra, rulo balya makinaları, yatay ve dikey yem karma makinaları, kendi yürür ve sıra bağımsız silaj makinaları, toprak işleme makinaları, silaj paketleme makinaları üretimlerini de bünyesine katmış bulunmaktadır.', _textStyle14),
+              _container('Paksan, Türkiye genelinde bulunan geniş bayi ve servis ağı sayesinde doğudan batıya, kuzeyden güneye tüm müşterilerine en kısa sürede ulaşabilmektedir. Bugün 6 kıtaya ihracat yapan Paksan Türkiye’de ve dünyada yatırımlarına hız kesmeden devam etmektedir.', _textStyle14),
               Divider(),
-              _ctText4(),
-              _ctText5(),
-              _ctText6(),
-                _ctImageSayfaSonu(),
+              _container('Teknolojimizi Keşfedin', _textStyle32),
+              _container('Paksan ürünlerinde müşteri memnuniyetini arttırmak, sorunsuz, dayanıklı ve güçlü makinalar üretebilmek, üretim ve maliyeti azaltmak, uygulanabilir yeni teknolojileri kullanmak amacıyla araştırma ve geliştirme faaliyetlerine oldukça önem vermektedir. Müşterilerine mükemmel ürünler ve hizmetler sunabilmek amacıyla en son teknolojik gelişmeleri üretimde kullanmak,', _textStyle14),
+              _container('tüm çalışmalarda güncel, kabul görmüş bilgi işlem yazılımlarından faydalanmak için yatırımlarına hız kesmeden devam etmektedir. Toplamda 26.000 m2 bulan üretim tesislerinde yatay ve dikey işleme merkezleri, CNC ve yarı otomatik tornalar, freze, broş, azdırma, robot kaynak makinaları, robot abgand, CNC lazer kesim, eksantrik ve hidrolik pres gibi oldukça geniş bir tezgah parkına sahiptir.', _textStyle14),
+              _ctImageSayfaSonu(),
             ],
           ),
         ),
@@ -30,126 +30,10 @@ class AboutUs extends StatelessWidget {
 
   Container _ctImageSayfaSonu() {
     return Container(
-        padding: EdgeInsets.fromLTRB(00, 25, 00, 00),
-        child: Image.asset('images/FSon.jpg',),
-      );
-  }
-  Container _ctText7() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
-      child: Text(
-        'Paksan her geçen gün ürün yelpazesini geliştirmeye devam etmektedir. Prizmatik balya makinası üretiminin yanı sıra, rulo balya makinaları, yatay ve dikey yem karma makinaları, kendi yürür ve sıra bağımsız silaj makinaları, toprak işleme makinaları, silaj paketleme makinaları üretimlerini de bünyesine katmış bulunmaktadır.',
-        style: TextStyle(
-          fontSize: 14,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-          color: Colors.grey[600],
-        ),
+      padding: EdgeInsets.fromLTRB(00, 25, 00, 00),
+      child: Image.asset(
+        'images/FSon.jpg',
       ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-  Container _ctText6() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 28, 10, 00),
-      // padding: EdgeInsets.fromLTRB(left, top, right, bottom)
-      child: Text(
-        'tüm çalışmalarda güncel, kabul görmüş bilgi işlem yazılımlarından faydalanmak için yatırımlarına hız kesmeden devam etmektedir. Toplamda 26.000 m2 bulan üretim tesislerinde yatay ve dikey işleme merkezleri, CNC ve yarı otomatik tornalar, freze, broş, azdırma, robot kaynak makinaları, robot abgand, CNC lazer kesim, eksantrik ve hidrolik pres gibi oldukça geniş bir tezgah parkına sahiptir.',
-        style: TextStyle(
-          fontSize: 14,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-          color: Colors.grey[600],
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-
-  Container _ctText5() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 28, 10, 00),
-      child: Text(
-        'Paksan ürünlerinde müşteri memnuniyetini arttırmak, sorunsuz, dayanıklı ve güçlü makinalar üretebilmek, üretim ve maliyeti azaltmak, uygulanabilir yeni teknolojileri kullanmak amacıyla araştırma ve geliştirme faaliyetlerine oldukça önem vermektedir. Müşterilerine mükemmel ürünler ve hizmetler sunabilmek amacıyla en son teknolojik gelişmeleri üretimde kullanmak,',
-        style: TextStyle(
-          fontSize: 14,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-          color: Colors.grey[600],
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-
-  Container _ctText4() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
-      child: Text(
-        'Teknolojimizi Keşfedin',
-        style: TextStyle(
-          fontSize: 32,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-          color: Colors.grey[600],
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-
-  Container _ctText3() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
-      child: Text(
-        'Paksan, Türkiye genelinde bulunan geniş bayi ve servis ağı sayesinde doğudan batıya, kuzeyden güneye tüm müşterilerine en kısa sürede ulaşabilmektedir. Bugün 6 kıtaya ihracat yapan Paksan Türkiye’de ve dünyada yatırımlarına hız kesmeden devam etmektedir.',
-        style: TextStyle(
-          fontSize: 14,
-          color: Colors.grey[600],
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-
-  Container _ctText2() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
-      child: Text(
-        '1970 yılında Özhan Pak ve ortakları tarafından kurulmuş olan Paksan Makina bugün Türkiye’nin ve dünyanın en önemli balya makinası üreticileri arasında yer almaktadır. Paksan toplamda 21.000 m2 kapalı toplam 62.000 m2’yi bulan üretim tesislerinde, robotlar, modern CNC tezgahları, son model lazer kesim makinaları ve en gelişmiş çizim programları gibi son teknoloji imkanlarını kullanarak dünya standartlarında makina üretmektedir.',
-        style: TextStyle(
-          fontSize: 14,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.9,
-          color: Colors.grey[600],
-        ),
-      ),
-      alignment: Alignment.centerLeft,
-    );
-  }
-
-  Container _ctText1() {
-    return Container(
-      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
-      child: Text(
-        '6 kıtaya ihracat yapan Paksan Türkiye’de ve dünyada yatırımlarına hız kesmeden devam ediyor.',
-        style: TextStyle(
-          fontSize: 32,
-          wordSpacing: 0.5,
-          letterSpacing: 1,
-          height: 1.2,
-          color: Colors.grey[600],
-        ),
-      ),
-      alignment: Alignment.centerLeft,
     );
   }
 
@@ -157,8 +41,6 @@ class AboutUs extends StatelessWidget {
     return Stack(
       children: <Widget>[
         Container(
-          // padding: EdgeInsets.fromLTRB(0, 0, 00, 20),
-          // padding: EdgeInsets.fromLTRB(left, top, right, bottom)
           child: Image.asset(
             'images/tarla.jpg',
             scale: 1,
@@ -166,10 +48,35 @@ class AboutUs extends StatelessWidget {
         ),
         Container(
           padding: EdgeInsets.fromLTRB(20, 40, 20, 00),
-          child: Image.asset('images/PAKSAN.png', scale: 0.63),
+          child: Image.asset('images/PAKSAN.png', scale: 0.90,),
         ),
       ],
     );
   }
 }
-//
+
+//25.12.2019
+Widget _container( String text , TextStyle _textStyle ) => Container(
+      padding: EdgeInsets.fromLTRB(10, 30, 10, 00),
+      child: Text( text,
+      style: _textStyle),
+      alignment: Alignment.centerLeft,
+    );
+
+
+
+final _textStyle32 = TextStyle(
+  fontSize: 32,
+  wordSpacing: 0.5,
+  letterSpacing: 1,
+  height: 1.2,
+  color: Colors.grey[600],
+);
+
+final _textStyle14 = TextStyle(
+  fontSize: 14,
+  wordSpacing: 0.5,
+  letterSpacing: 1,
+  height: 1.2,
+  color: Colors.grey[600],
+);
--- lib/buyukbalya.dart
+++ lib/buyukbalya.dart
@@ -1,3 +1,5 @@
+//import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:first_app/main.dart';
 
@@ -13,12 +15,11 @@ class BuyukBalya extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              _textBuyukBalyaMakinalari(),
-              _stackOrkinos870(context),
-              _stackOrkinos1270(context),
-              Container(
-                child: Image.asset('images/FSon.jpg'),
-              ),
+              _headerText("Büyük Balya Makinaları"),
+              _urunlerWidget(context, "/buyukbalya","images/Orkinos.jpg", "Orkinos 1270", "Büyük Balya Makinaları"),
+              _urunlerWidget(context, "/buyukbalya","images/Orkinos870X-1.jpg", "Orkinos 870", "Büyük Balya Makinaları"),
+              _urunlerWidget(context, "/buyukbalya","images/Orkinos870X-1.jpg", "Orkinos 888", "Deneme"),
+              _imageAsset('images/FSon.jpg'),
             ],
           ),
         ),
@@ -27,100 +28,49 @@ class BuyukBalya extends StatelessWidget {
   }
 }
 
-  Stack _stackOrkinos1270(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 22, 10, 00),
-          // padding: EdgeInsets.fromLTRB(left, top, right, bottom)
-          child: FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "/buyukbalya");              
-            },
-            child: Image.asset(
-              'images/Orkinos.jpg',
-              scale: 3,
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 332, 10, 00),
-          child: Text(
-            'Orkinos 1270',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.blue[900],
-            ),
-          ),
-          alignment: Alignment.center,
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 362, 10, 00),
-          child: Text(
-            'Büyük Balya Makinaları',
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.blue[700],
-            ),
-          ),
-          alignment: Alignment.center,
-        ),
-      ],
-    );
-  }
-  Stack _stackOrkinos870(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 22, 10, 00),
-          // padding: EdgeInsets.fromLTRB(left, top, right, bottom)
-          child: FlatButton(
-            onPressed: () {
-              Navigator.pushNamed(context, "/buyukbalya");              
-            },
-            child: Image.asset(
-              'images/Orkinos870X-1.jpg',
-              scale: 3,
-            ),
-          ),
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 332, 10, 00),
-          child: Text(
-            'Orkinos 870',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.blue[900],
-            ),
-          ),
-          alignment: Alignment.center,
-        ),
-        Container(
-          padding: EdgeInsets.fromLTRB(10, 362, 10, 00),
-          child: Text(
-            'Büyük Balya Makinaları',
-            style: TextStyle(
-              fontSize: 14,
-              color: Colors.blue[700],
-            ),
-          ),
-          alignment: Alignment.center,
-        ),
-      ],
-    );
-  }
-    Container _textBuyukBalyaMakinalari() {
-    return Container(
+Widget _urunlerWidget(BuildContext context,String _url , String _imageUrl, String _productName ,String _catagoryName )=> Stack(children: <Widget>[
+      _mainContainer(context, _url,_imageUrl),
+      _container(_productName, _textStyle14, _edgeInsets332),
+      _container(_catagoryName, _textStyle14, _edgeInsets362)
+],);
 
-      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-      color: Colors.grey[300],
-      child: Text(
-        'Büyük Balya Makinaları',
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: Colors.black87,
-          fontSize: 26,
-        ),
+
+Widget _headerText(String text) => Container(
+  padding: _edgeInsetsSymmetric,
+  color: Colors.grey[300],
+  child: Text(text, textAlign: TextAlign.center,style: _textstyle26),
+);
+
+Widget _mainContainer( BuildContext context, String _url , String _imageUrl) => Stack(
+ children: <Widget>[
+      Container(
+         padding: EdgeInsets.fromLTRB(10, 22, 10, 00),
+     child: _flatButton(context,_url,_imageUrl ),
       ),
+     ]
+);
+Widget _container(String text, TextStyle _textStyle, EdgeInsets _edgeInsets) =>
+    Container(
+      padding: _edgeInsets,
+      child: Text(text, style: _textStyle),
+      alignment: Alignment.center,
     );
-  }
+
+Widget _imageAsset( String _url  ) => Image.asset( _url, scale: 3,);
+Widget _flatButton( BuildContext context, String _url , String _imageUrl) => FlatButton(
+          onPressed: () {
+            Navigator.pushNamed(context, _url);
+          }, 
+           child: _imageAsset(_imageUrl)
+          );
+final _textStyle14 = TextStyle(
+  fontSize: 14,
+  color: Colors.blue[700],);
+
+final _textstyle26 = TextStyle(
+        color: Colors.black87,
+        fontSize: 26,);
+
+final _edgeInsets332 = EdgeInsets.fromLTRB(10, 332, 10, 00);
+final _edgeInsets362 = EdgeInsets.fromLTRB(10, 362, 10, 00);
+final _edgeInsetsSymmetric = EdgeInsets.symmetric(horizontal: 0, vertical: 20);
